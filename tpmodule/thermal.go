b25d09633feec3a4ff37652808cd3b1328013ad4
Gianguido Sorà <g.sora4@gmail.com>
2016-03-22 01:35:05 +0100

    cpu thermal readings by thinkpad_acpi

---
+++ tpmodule/thermal.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+// GetCPUTemp returns the CPU temperature as read by thinkpad_acpi
+func GetCPUTemp() int {
+	dat, _ := ioutil.ReadFile("/proc/acpi/ibm/thermal")
+	content := strings.Split(string(dat), " ")
+	content = strings.Split(content[0], ":")
+	temp := strings.TrimSpace(content[1])
+	k, _ := strconv.Atoi(temp)
+	return k
+}
+
+func main() {
+	fmt.Println(GetCPUTemp())
+}
