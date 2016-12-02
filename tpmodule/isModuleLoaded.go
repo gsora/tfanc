3f361e313402e5dac5cd5fb1c66d39a0f53e95f5
Gianguido Sora` <g.sora4@gmail.com>
2016-12-02 18:25:14 +0000

    a more correct syntax, fan_control detection now works

--- tpmodule/isModuleLoaded.go
+++ tpmodule/isModuleLoaded.go
@@ -3,31 +3,28 @@ package tpmodule
 import (
 	"errors"
 	"io/ioutil"
+	"log"
+	"strings"
 )
 
 // IsModuleLoaded checks if the kernel module is loaded with fan_control=1, else exit
 func IsModuleLoaded() error {
-	basePath := "/sys/module/"
-	modList, _ := ioutil.ReadDir(basePath)
+	basePath := "/sys/module/thinkpad_acpi"
+	_, err := ioutil.ReadDir(basePath)
 
-	found := false
-
-	for _, module := range modList {
-		if module.Name() == "thinkpad_acpi" {
-			found = true
-			break
-		}
+	if err != nil {
+		return errors.New("thinkpad_acpi module not loaded.")
 	}
 
-	if found == false {
-		return errors.New("thinkpad_acpi error: module not loaded")
+	fanControlFile, err := ioutil.ReadFile(basePath + "/parameters/fan_control")
+
+	if err != nil {
+		log.Fatal(err)
+
 	}
+	fanControlFlag := strings.TrimSpace(string(fanControlFile))
 
-	basePath = basePath + "thinkpad_acpi/parameters/"
-	fanControlFile, _ := ioutil.ReadFile(basePath + "fan_control")
-	fanControlFlag := string(fanControlFile)
-
-	if fanControlFlag == "N" {
+	if fanControlFlag != "Y" {
 		return errors.New("thinkpad_acpi error: module loaded, but not initialized with \"fan_control=1\" parameter")
 	}
 
